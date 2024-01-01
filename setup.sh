092a6d9398f9b8dac0267ea5d704b7bc41ae21c5
Jon Ganz <jonganz.dev@gmail.com>
2023-12-31 18:25:38 -0600

    updating repo just in case it was already checked out

--- setup.sh
+++ setup.sh
@@ -3,6 +3,10 @@
 # Clone the repository
 git clone https://github.com/JonGanz/server-setup.git
 cd server-setup
+git fetch -p
+git reset --hard
+git checkout main
+git rebase
 
 # Install Ansible.
 apt update
