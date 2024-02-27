071f2e8cd4186e81f9d97da2288b342cc977d3a3
Jon Ganz <jonganz.dev@gmail.com>
2024-02-26 22:23:16 -0600

    added tandoor playbook to update script; duh doi...

--- update.sh
+++ update.sh
@@ -4,4 +4,5 @@ ansible-playbook -i localhost, -c local server-setup.yml
 ansible-playbook -i localhost, -c local nfs-mounts.yml
 
 ansible-playbook -i localhost, -c local ./valheim/deploy.yml
+ansible-playbook -i localhost, -c local ./tandoor/deploy.yml
 
